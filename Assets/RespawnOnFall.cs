4586395425e99aae4059cfde5f1b0e1a237e6391
sofiigrance <sofiigrance@gmail.com>
2025-05-19 09:01:59 -0300

    Parkour

---
+++ Assets/RespawnOnFall.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RespawnOnFall : MonoBehaviour
+{
+    public float yRespawnValue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y < yRespawnValue)
+        {
+            //Debug.Log("Me caí :(");
+            SceneManager.LoadScene("FPS Parkour");
+        }
+    }
+}
