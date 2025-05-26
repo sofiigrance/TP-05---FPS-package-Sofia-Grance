0f70f8ca33309ca43e0db5d5bd20a99335845c8b
sofiigrance <sofiigrance@gmail.com>
2025-05-26 08:49:56 -0300

    Cajaybarrildesaparece

---
+++ Assets/Script/DeteccionDeCollisiones.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeteccionDeCollisiones : MonoBehaviour
+{
+    // Start is called before the first frame update
+     private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Contacto con" + collision.gameObject.name);
+        if (collision.gameObject.name == "Player")
+        {
+            Destroy(gameObject);
+        }
+        
+    }   
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
